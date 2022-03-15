d2ef1d979ca4570eb1557d7039eb75fcc60fa6b8
Luke Bemish <lukebemish@gmail.com>
2022-03-15 15:21:56 -0500

    Allow maxDamage and stack size

--- common/src/main/java/com/github/lukebemish/clojurewrapper/wrappers/ExtendedItemProperties.java
+++ common/src/main/java/com/github/lukebemish/clojurewrapper/wrappers/ExtendedItemProperties.java
@@ -10,17 +10,25 @@ public class ExtendedItemProperties extends Item.Properties {
     public static Item.Properties props(IFn getter) {
         return new ExtendedItemProperties().durabilityOrElse((Integer) getter.invoke("max-damage",0))
                 .setFireRes((Boolean) getter.invoke("fire-resistant",false))
-                .stacksTo((Integer) getter.invoke("stack-size",64))
+                .stacksOrElse((Integer) getter.invoke("stack-size",64))
                 .tab((CreativeModeTab) getter.invoke("tab",null))
                 .rarity((Rarity) getter.invoke("rarity", Rarity.COMMON))
                 .food((FoodProperties) getter.invoke("food",null));
     }
 
+    int maxDamage;
+
     public ExtendedItemProperties durabilityOrElse(int maxDamage) {
+        this.maxDamage = maxDamage;
         return maxDamage==0?this:(ExtendedItemProperties)this.durability(maxDamage);
     }
 
     public ExtendedItemProperties setFireRes(boolean isRes) {
         return isRes?(ExtendedItemProperties)this.fireResistant():this;
+    }
+
+    public ExtendedItemProperties stacksOrElse(int stackSize) {
+        if (stackSize == 1 && this.maxDamage > 0) return this;
+        return (ExtendedItemProperties) this.stacksTo(stackSize);
     }
 }
