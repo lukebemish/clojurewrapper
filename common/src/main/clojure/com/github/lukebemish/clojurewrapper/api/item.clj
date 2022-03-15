75dad4cd6ea2478af98ef5eae3f17866831787a3
Luke Bemish <lukebemish@gmail.com>
2022-03-15 15:25:19 -0500

    Remove unused item property extender

--- common/src/main/clojure/com/github/lukebemish/clojurewrapper/api/item.clj
+++
@@ -1,4 +0,0 @@
-(ns com.github.lukebemish.clojurewrapper.api.item
-  (:import (com.github.lukebemish.clojurewrapper.wrappers ExtendedItemProperties)))
-
-(defn item-properties [mapping] (ExtendedItemProperties/props (fn [string default] (get mapping (keyword string) default))))
