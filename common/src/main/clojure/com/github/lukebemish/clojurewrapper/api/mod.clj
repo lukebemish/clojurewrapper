a6bf81f02ab310a76e5650e62936b4b92a5d6de8
Luke Bemish <lukebemish@gmail.com>
2022-04-01 20:54:37 -0600

    Reorder mod-load

--- common/src/main/clojure/com/github/lukebemish/clojurewrapper/api/mod.clj
+++ common/src/main/clojure/com/github/lukebemish/clojurewrapper/api/mod.clj
@@ -4,12 +4,14 @@
 
 (defn mod-load [mod-vals]
   (do
+    (if (not (nil? (mod-vals :before-registries)))
+      (apply (mod-vals :before-registries) '()))
+    (if (not (nil? (mod-vals :registries)))
+      (platform/run-platform 'com.github.lukebemish.clojurewrapper.impl.registries/register-objects
+                             (mod-vals :registries)))
+    (if (not (nil? (mod-vals :main)))
+      (apply (mod-vals :main) '()))
     (if (and (sided/client?) (not (nil? (mod-vals :client))))
       (apply (mod-vals :client) '()))
     (if (and (sided/server?) (not (nil? (mod-vals :server))))
-      (apply (mod-vals :server) '()))
-    (if (not (nil? (mod-vals :main)))
-      (apply (mod-vals :main) '()))
-    (if (not (nil? (mod-vals :registries)))
-      (platform/run-platform 'com.github.lukebemish.clojurewrapper.impl.registries/register-objects
-                             (mod-vals :registries)))))
+      (apply (mod-vals :server) '()))))
